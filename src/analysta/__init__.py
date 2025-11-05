90148e3a114d37817cd4df17c0b2ae358019c5ac
Diego-Ignacio Ortiz <31400790+dunkel000@users.noreply.github.com>
2025-11-04 22:49:02 -0300

    Add CSV and Excel I/O helpers

--- src/analysta/__init__.py
+++ src/analysta/__init__.py
@@ -6,7 +6,18 @@
 
 from .delta import Delta
 from .diff import hello, trim_whitespace, find_duplicates
+from .io import read_csv, write_csv, read_excel, write_excel
 from .quality import audit_dataframe
 from .__about__ import __version__
 
-__all__ = ["Delta", "hello", "trim_whitespace", "find_duplicates", "audit_dataframe"]
+__all__ = [
+    "Delta",
+    "hello",
+    "trim_whitespace",
+    "find_duplicates",
+    "audit_dataframe",
+    "read_csv",
+    "write_csv",
+    "read_excel",
+    "write_excel",
+]
